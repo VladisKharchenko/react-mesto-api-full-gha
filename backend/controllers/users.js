913fabe89626b4bb1ab23f1b00d5a99fb9702e78
Владислав Харченко <120054604+VladisKharchenko@users.noreply.github.com>
2023-08-04 20:28:29 +0400

    Проект от 04.08.2023-4

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const NotFoundError = require('../errors/not-found-err');
@@ -6,6 +7,7 @@ const CustomError = require('../errors/custom-err');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
 const CREATED_SUCCESSFULLY = 201;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUsers = async (req, res, next) => {
   try {
@@ -124,7 +126,7 @@ const login = async (req, res, next) => {
 
     const payload = { _id: user._id };
 
-    const token = jwt.sign(payload, 'some-secret-key', { expiresIn: '1w' });
+    const token = jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '1w' });
 
     res.cookie('jwt', token, { httpOnly: true, maxAge: 604800000, sameSite: true });
 
