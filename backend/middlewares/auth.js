bfdbee9f752f4af7cb94872043044b493d26c8b2
Владислав Харченко <120054604+VladisKharchenko@users.noreply.github.com>
2023-08-07 11:50:20 +0400

    Проект от 07.08.2023

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const authMiddleware = async (req, res, next) => {
   const token = req.cookies.jwt;
 
   try {
-    const payload = await jwt.verify(token, 'some-secret-key');
+    const payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
     req.user = payload;
     return next();
   } catch (error) {
